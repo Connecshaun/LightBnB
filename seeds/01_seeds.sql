ed5cc4f3a4280cd30229bcbc520590456ba31842
Shaun Robinson <connecshaun@gmail.com>
2022-01-18 02:57:28 +0000

    Update seed data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -10,20 +10,20 @@ VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/208667
 (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 852, 6, 6, 7, 'Canada', '651 Nami Road', 'Calgary', 'Alberta', '83680', true),
 (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 460, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Edmonton', 'Alberta', '44583', true),
 (4, 'Headed know', 'description', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg', 826, 0, 5, 5, 'Canada', '513 Powov Grove', 'Arnprior', 'Ontario', '38051', true),
-(6, 'Port out', 'description', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg', 483, 2, 8, 0, 'Canada', '1392 Gaza Junction', 'Winnipeg', 'Manitoba', '81059', true);
+(5, 'Port out', 'description', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg', 483, 2, 8, 0, 'Canada', '1392 Gaza Junction', 'Winnipeg', 'Manitoba', '81059', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 2, 1),
 ('2019-01-04', '2019-02-01', 2, 1),
 ('2023-05-27', '2023-05-28', 4, 2),
 ('2016-07-17', '2016-08-01', 3, 4),
-('2015-09-13', '2015-09-30', 6, 6);
+('2015-09-13', '2015-09-30', 5, 5);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES ( 1, 4, 1, 4, 'messages'),
 (2, 5, 4, 3, 'messages'),
 (4, 2, 3, 5, 'messages'),
-(6, 2, 5, 5, 'messages'),
+(3, 2, 5, 5, 'messages'),
 (1, 2, 2, 5, 'messages');
 
 
