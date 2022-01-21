f3fb71621f4ab6afa707866f6aa9d8a8c4f79f6a
Shaun Robinson <connecshaun@gmail.com>
2022-01-21 22:41:48 +0000

    Update getUserWithId function to capture a single user from the database given their ID and clean up code for getUserWithEmail

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -24,7 +24,9 @@ const pool = new Pool({
 const getUserWithEmail = 
 function (email) {
   return pool
-  .query(`SELECT * FROM users WHERE email = $1`, ([email]))
+  .query(`SELECT * FROM users 
+  WHERE email = $1`, 
+  [email])
   .then ((result) => {
     // console.log(result.rows[0]);
     return result.rows[0];
@@ -40,8 +42,21 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+// const getUserWithId = function(id) {
+//   return Promise.resolve(users[id]);
+// }
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+  .query(
+    `SELECT * FROM users 
+    WHERE id = $1`, 
+    [id])
+    .then ((result) => {
+      return result.rows[0]
+    })
+    .catch((err) => {
+      return null;
+    })
 }
 exports.getUserWithId = getUserWithId;
 
